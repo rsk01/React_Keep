97fb9f24644119884d4c3dfb4036f6257dac8493
rsk01 <saikrishna.ent2@gmail.com>
2019-08-07 19:23:00 +0530

    backend added

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,4 @@
-// /backend/data.js
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
--- backend/server.js
+++ backend/server.js
@@ -10,37 +10,30 @@ const app = express();
 app.use(cors());
 const router = express.Router();
 
-// this is our MongoDB database
+
 const dbRoute =
   'mongodb+srv://rsk:1234@cluster0-v18f4.gcp.mongodb.net/test?retryWrites=true&w=majority';
 
-// connects our back end code with the database
+
 mongoose.connect(dbRoute, { useNewUrlParser: true });
 
 let db = mongoose.connection;
 
 db.once('open', () => console.log('connected to the database'));
 
-// checks if connection with the database is successful
+
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// (optional) only made for logging and
-// bodyParser, parses the request body to be a readable json format
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-// this is our get method
-// this method fetches all available data in our database
 router.get('/getData', (req, res) => {
   Data.find((err, data) => {
     if (err) return res.json({ success: false, error: err });
     return res.json({ success: true, data: data });
   });
 });
-
-// this is our update method
-// this method overwrites existing data in our database
 router.put('/updateData', (req, res) => {
   const { id, update } = req.body;
   Data.findByIdAndUpdate(id, update, (err) => {
@@ -48,9 +41,6 @@ router.put('/updateData', (req, res) => {
     return res.json({ success: true });
   });
 });
-
-// this is our delete method
-// this method removes existing data in our database
 router.delete('/deleteData', (req, res) => {
   const { id } = req.body;
   Data.findByIdAndRemove(id, (err) => {
@@ -59,8 +49,6 @@ router.delete('/deleteData', (req, res) => {
   });
 });
 
-// this is our create methid
-// this method adds new data in our database
 router.post('/putData', (req, res) => {
   let data = new Data();
 
@@ -81,8 +69,6 @@ router.post('/putData', (req, res) => {
   });
 });
 
-// append /api for our http requests
 app.use('/api', router);
 
-// launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
